9adf415bee5ae469b42b62e66575f3b6ab1878d4
Sholum-Ctrl <113040060+Sholum-Ctrl@users.noreply.github.com>
2022-09-17 21:54:16 +0700

    Add files via upload

---
+++ bank .py
@@ -0,0 +1,14 @@
+#Lecture3 Assignment
+
+answer = input("Greeting : ")#.strip().lower()
+
+if answer == "what's happening?":
+    print("$100")
+elif answer == "hello, Newman?" or answer == "hello":
+    print("$0")
+elif answer == "how you doing?":
+    print("$20")
+#elif answer == "What's happening? ":
+    #print("$100")
+else:
+    print("$0") 
